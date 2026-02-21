82befd1370023a613adeb5a6c8d9e9b8810b86ad
Ivan Cardenas <ivan.cardenas101@gmail.com>
2026-02-21 18:45:40 -0400

    Remove deprecated Terraform configuration files and outputs

--- base_config/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  resource_group_name = "${var.project_name}-${var.environment}-rg"
-  
-  common_tags = {
-    Project     = var.project_name
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
