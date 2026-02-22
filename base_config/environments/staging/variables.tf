d36dbac0d2a81c6b6b68a2ac79d427f95f316668
Ivan Cardenas <ivan.cardenas101@gmail.com>
2026-02-22 16:08:20 -0400

    Refactor variable formatting for consistency across dev, prod, and staging environments, and update compute module variable definitions for improved readability.

--- base_config/environments/staging/variables.tf
+++ base_config/environments/staging/variables.tf
@@ -3,10 +3,10 @@ variable "project_name" {
   type    = string
   default = "terraform-demo"
 }
-variable "environment"     { type = string }
+variable "environment" { type = string }
 variable "location" {
   type    = string
   default = "East US"
 }
-variable "vm_size"         { type = string }
+variable "vm_size" { type = string }
 variable "vnet_address_space" { type = list(string) }
--- base_config/modules/compute/variables.tf
+++ base_config/modules/compute/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "subnet_id"           { type = string }
-variable "vm_name"             { type = string }
-variable "vm_size"             { 
-    type = string
-    default = "Standard_B1s" 
-    }
-variable "tags"                { type = map(string) }
+variable "location" { type = string }
+variable "subnet_id" { type = string }
+variable "vm_name" { type = string }
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+variable "tags" { type = map(string) }
